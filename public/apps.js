3a4fbcb8edfa0fdad8480006c7810196f986a3e9
Peachiia <pongsakorn.wec@gmail.com>
2018-12-09 23:23:26 +0700

    Server with plain-text response

--- public/apps.js
+++ public/apps.js
@@ -1,34 +1,11 @@
-var fs = require('fs');
+var http = require('http')
 
+var server = http.createServer(function(req, res) {
+    console.log('>> Request incoming...')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from Plain Text Response');
+    console.log('<< Responded.')
+})
 
-// Sync
-var file = fs.readFileSync('read.txt', 'utf8');
-console.log(file)
-
-fs.writeFileSync('write.txt', file);
-console.log('Write Done!')
-
-
-// Async
-var file = fs.readFile('read.txt', 'utf8', function(err, data) {
-    console.log(data)
-
-    fs.writeFileSync('write.txt', data);
-    console.log('Write Done!');
-});
-
-
-// File Deletion
-console.log('Deleting...');
-fs.unlinkSync('write.txt');
-console.log('Deleting DONE!');
-
-// Make Directory
-// Sync ** can call only once, th secind time will be Error: EEXIST: file already exists
-//fs.mkdirSync('new_folder');
-
-// Async
-fs.mkdir('new_folder', function() {
-    fs.writeFileSync('./new_folder/write_in_folder.txt', fs.readFileSync('read.txt', 'utf8'));
-    console.log('Write in Folder Done!')
-});
+server.listen(3000, '127.0.0.1');
+console.log('Start listening on port 3000');
